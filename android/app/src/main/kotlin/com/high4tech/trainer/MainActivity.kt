09f71ede3897811284a90821bc2891178c6b77ef
HAMZA-CH420 <hakram554r@gmail.com>
2026-03-24 17:46:43 +0500

    first commit

---
+++ android/app/src/main/kotlin/com/high4tech/trainer/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.high4tech.trainer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/UIhelper/colorPalatte/color_palette.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class Palette{
+  Color primaryColor = Colors.white;
+}
