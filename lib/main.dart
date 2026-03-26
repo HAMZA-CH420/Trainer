903709201ba2e40134fdbb1c619322c6d3080e2c
HAMZA-CH420 <hakram554r@gmail.com>
2026-03-27 00:44:55 +0500

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:trainer/Features/AuthenticationScreens/SignUpScreen/goal_description_screen.dart';
-import 'package:trainer/Features/OnboardingScreens/splashScreen/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
