0327e1d8998666a948c809e4fe5fa292ac31c7ed
HAMZA-CH420 <hakram554r@gmail.com>
2026-03-26 21:18:13 +0500

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:trainer/Features/AuthenticationScreens/SignUpScreen/verification_screen.dart';
 import 'package:trainer/Features/OnboardingScreens/splashScreen/splash_screen.dart';
 
 void main() {
