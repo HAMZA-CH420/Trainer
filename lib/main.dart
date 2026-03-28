d7e08f777a057540a8add94c8619a0e55e54a9aa
HAMZA-CH420 <hakram554r@gmail.com>
2026-03-28 21:50:31 +0500

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:trainer/Features/OnboardingScreens/splashScreen/splash_screen.dart';
 
-void main() {
+void main() async {
+  //firebase integration
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
