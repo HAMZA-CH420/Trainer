0e9e11ba62227858dbbc5df9d85abd56b3de6dcb
HAMZA-CH420 <hakram554r@gmail.com>
2026-03-31 14:28:03 +0500

    changes

---
+++ lib/Features/BottomNavBar/bottom_nav_bar.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class BottomNavBar extends StatefulWidget {
+  const BottomNavBar({super.key});
+
+  @override
+  State<BottomNavBar> createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> {
+  @override
+  Widget build(BuildContext context) {
+    int primaryIndex = 0;
+    final List<Widget> screens = [];
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(items: []),
+      body: Container(),
+    );
+  }
+}
