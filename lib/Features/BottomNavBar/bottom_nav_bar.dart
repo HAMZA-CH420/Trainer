7a20b6678f5d3b1bd14918c01c6929f3a8535a67
HAMZA-CH420 <hakram554r@gmail.com>
2026-03-31 14:37:28 +0500

    changes

--- lib/Features/BottomNavBar/bottom_nav_bar.dart
+++ lib/Features/BottomNavBar/bottom_nav_bar.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:trainer/Features/DashboardScreen/view/dashboard_screen.dart';
+import 'package:trainer/Features/ScheduleScreen/view/schedule_screen.dart';
 
 class BottomNavBar extends StatefulWidget {
   const BottomNavBar({super.key});
@@ -11,7 +13,7 @@ class _BottomNavBarState extends State<BottomNavBar> {
   @override
   Widget build(BuildContext context) {
     int primaryIndex = 0;
-    final List<Widget> screens = [];
+    final List<Widget> screens = [DashboardScreen(), ScheduleScreen()];
     return Scaffold(
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: primaryIndex,
