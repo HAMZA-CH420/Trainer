77dade4491b36821264f1d3472f44c9c63bf6058
HAMZA-CH420 <hakram554r@gmail.com>
2026-04-01 12:33:32 +0500

    changes

--- lib/Features/DashboardScreen/view/dashboard_screen.dart
+++ lib/Features/DashboardScreen/view/dashboard_screen.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:trainer/UIhelper/colorPalette/color_palette.dart';
 
 class DashboardScreen extends StatelessWidget {
   const DashboardScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text("Dashboard")));
+    return Scaffold(
+      appBar: AppBar(
+        leading: Icon(Icons.menu, color: Palette.primaryColor),
+        title: Text(
+          "Dashboard",
+          style: GoogleFonts.poppins(fontWeight: FontWeight.bold, fontSize: 22),
+        ),
+        centerTitle: true,
+      ),
+    );
   }
 }
