36e591a8bda99592f09e30d056cf1b24d2fa88a4
HAMZA-CH420 <hakram554r@gmail.com>
2026-04-02 18:41:56 +0500

    changes

--- lib/Features/DashboardScreen/view/FindTrainerScreen/widgets/custom_search_bar.dart
+++ lib/Features/DashboardScreen/view/FindTrainerScreen/widgets/custom_search_bar.dart
@@ -1,15 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:trainer/UIhelper/colorPalette/color_palette.dart';
 
-class CustomSearchBar extends StatelessWidget {
+class CustomSearchBar extends StatefulWidget {
   const CustomSearchBar({super.key});
+
+  @override
+  State<CustomSearchBar> createState() => _CustomSearchBarState();
+}
+
+class _CustomSearchBarState extends State<CustomSearchBar> {
+  TextEditingController controller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.sizeOf(context);
     return Container(
-      height: size.height / 14,
+      height: size.height * .06,
       width: size.width,
-      decoration: BoxDecoration(color: Colors.black),
+
+      padding: EdgeInsets.only(left: 10, top: 5),
+      decoration: BoxDecoration(
+        color: Color(0xFFE8EDF5),
+        borderRadius: BorderRadius.circular(13),
+      ),
+      child: TextField(
+        decoration: InputDecoration(
+          border: InputBorder.none,
+          prefixIcon: Icon(Icons.search, color: Palette.primaryColor, size: 34),
+          hintText: "Search for trainers",
+        ),
+      ),
     );
   }
 }
