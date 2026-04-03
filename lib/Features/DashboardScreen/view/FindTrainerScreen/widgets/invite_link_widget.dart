36fe103d3416d7cf63dcb950936bea159e8a18fc
HAMZA-CH420 <hakram554r@gmail.com>
2026-04-03 20:43:45 +0500

    changes

--- lib/Features/DashboardScreen/view/FindTrainerScreen/widgets/invite_link_widget.dart
+++ lib/Features/DashboardScreen/view/FindTrainerScreen/widgets/invite_link_widget.dart
@@ -1,22 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:trainer/UIhelper/colorPalette/color_palette.dart';
 
 class InviteLinkWidget extends StatelessWidget {
-  const InviteLinkWidget({super.key});
+  InviteLinkWidget({super.key});
+
+  final TextEditingController controller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.sizeOf(context);
     return Container(
-      height: size.height * .06,
-      padding: EdgeInsets.symmetric(horizontal: 10),
+      height: size.height * .055,
+      alignment: Alignment.centerLeft,
+      padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
       decoration: BoxDecoration(
         color: Colors.white,
         borderRadius: BorderRadius.circular(10),
       ),
       child: TextField(
+        controller: controller,
+        maxLines: 1,
         decoration: InputDecoration(
           border: InputBorder.none,
           hintText: "invite link",
+          suffixIcon: Container(
+            width: size.width * .3,
+            decoration: BoxDecoration(
+              color: Palette.primaryColor,
+              borderRadius: BorderRadius.circular(5),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Icon(Icons.link, color: Colors.white),
+                Text(
+                  "Invite Link",
+                  style: GoogleFonts.poppins(color: Colors.white),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
