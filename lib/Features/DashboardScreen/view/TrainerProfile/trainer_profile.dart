281e5116744fbbafeec777cde73c478e772f6549
HAMZA-CH420 <hakram554r@gmail.com>
2026-04-04 22:15:37 +0500

    changes

---
+++ lib/Features/DashboardScreen/view/TrainerProfile/trainer_profile.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TrainerProfile extends StatelessWidget {
+  const TrainerProfile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
