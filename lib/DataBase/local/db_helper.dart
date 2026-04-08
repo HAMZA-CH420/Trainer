b438965efecc9f991337047a3fe1fac5433100d9
HAMZA-CH420 <hakram554r@gmail.com>
2026-04-08 19:40:33 +0500

    changes

--- lib/DataBase/local/db_helper.dart
+++ lib/DataBase/local/db_helper.dart
@@ -21,7 +21,7 @@ class LocalDataBase {
     }
   }
 
-  // a commit is needed here
+  // a commit is needed here to
   Future<Database> openDb() async {
     Directory appDir = await getApplicationDocumentsDirectory();
     String dirPath = join(appDir.path, "Trainer.db");
