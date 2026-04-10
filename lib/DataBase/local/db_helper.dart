112e8162a28b5d571981cd17c2fa55b01b8f7e80
HAMZA-CH420 <hakram554r@gmail.com>
2026-04-10 18:03:26 +0500

    changes

--- lib/DataBase/local/db_helper.dart
+++ lib/DataBase/local/db_helper.dart
@@ -45,7 +45,7 @@ class LocalDataBase {
   }) async {
     String userId =
         "${userName.toLowerCase().replaceAll(' ', '_')}_${DateTime.now().millisecondsSinceEpoch}";
-    //
+    //tt
     var db = await getDb();
     int rowsAffected = await db.insert("userList", {
       "userName": userName,
