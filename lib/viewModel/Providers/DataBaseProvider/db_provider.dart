1288cb88440f3d93ca2f5f5194f05deb94f3941c
HAMZA-CH420 <hakram554r@gmail.com>
2026-04-13 21:41:12 +0500

    changes

--- lib/viewModel/Providers/DataBaseProvider/db_provider.dart
+++ lib/viewModel/Providers/DataBaseProvider/db_provider.dart
@@ -78,7 +78,7 @@ class DbProvider extends ChangeNotifier {
     );
   }
 
-  ///get trainers in a list lala
+  ///get trainers in a list lala land
   Future<List<Map<String, dynamic>>> getTrainerProfiles() async {
     return await localDataBase.getTrainerProfile();
   }
