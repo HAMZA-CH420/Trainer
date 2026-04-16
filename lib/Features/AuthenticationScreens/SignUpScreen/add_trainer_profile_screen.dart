f7d4cc32c3b225277f0b9b1c82380c1c3690633b
HAMZA-CH420 <hakram554r@gmail.com>
2026-04-16 19:24:28 +0500

    changes

--- lib/Features/AuthenticationScreens/SignUpScreen/add_trainer_profile_screen.dart
+++ lib/Features/AuthenticationScreens/SignUpScreen/add_trainer_profile_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:trainer/Features/AuthenticationScreens/sharedWidgets/custom_text_field.dart';
 import 'package:trainer/Features/TraineeSide/DashboardScreen/widgets/dash_title.dart';
+import 'package:trainer/UIhelper/colorPalette/color_palette.dart';
 
 class AddTrainerProfileScreen extends StatefulWidget {
   const AddTrainerProfileScreen({super.key});
@@ -20,14 +21,26 @@ class _AddTrainerProfileScreenState extends State<AddTrainerProfileScreen> {
         title: DashTitle(title: "Update Profile"),
         centerTitle: true,
       ),
-      body: Column(
-        children: [
-          CustomTextField(
-            title: "Name",
-            hint: "Your Name",
-            controller: nameController,
-          ),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [
+            CircleAvatar(
+              radius: 50,
+              backgroundColor: Palette.primaryColor,
+              child: Icon(
+                Icons.add_a_photo_outlined,
+                color: Colors.white,
+                size: 38,
+              ),
+            ),
+            CustomTextField(
+              title: "Name",
+              hint: "Your Name",
+              controller: nameController,
+            ),
+          ],
+        ),
       ),
     );
   }
