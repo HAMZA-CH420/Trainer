ec2f80d7041e2f0e4fc5363e813a2558659f15f2
HAMZA-CH420 <hakram554r@gmail.com>
2026-04-16 20:01:49 +0500

    changes

--- lib/Features/AuthenticationScreens/SignUpScreen/add_trainer_profile_screen.dart
+++ lib/Features/AuthenticationScreens/SignUpScreen/add_trainer_profile_screen.dart
@@ -14,6 +14,7 @@ class AddTrainerProfileScreen extends StatefulWidget {
 class _AddTrainerProfileScreenState extends State<AddTrainerProfileScreen> {
   TextEditingController nameController = TextEditingController();
   TextEditingController specController = TextEditingController();
+  TextEditingController expController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -44,6 +45,11 @@ class _AddTrainerProfileScreenState extends State<AddTrainerProfileScreen> {
               title: "Specialization",
               hint: "i.e Strength trainer",
               controller: specController,
+            ),
+            CustomTextField(
+              title: "Experience",
+              hint: "time in years",
+              controller: expController,
             ),
           ],
         ),
