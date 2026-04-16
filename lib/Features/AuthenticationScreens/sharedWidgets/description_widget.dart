be42de1f62ec99829aaa91e8ee8229f534c768ea
HAMZA-CH420 <hakram554r@gmail.com>
2026-04-16 20:05:04 +0500

    changes

---
+++ lib/Features/AuthenticationScreens/sharedWidgets/description_widget.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class DescriptionWidget extends StatefulWidget {
+  const DescriptionWidget({
+    super.key,
+    required this.title,
+    required this.hint,
+    required this.controller,
+    this.validator,
+  });
+
+  final String title;
+  final String hint;
+  final TextEditingController controller;
+  final String? Function(String?)? validator;
+
+  @override
+  State<DescriptionWidget> createState() => _CustomTextFieldState();
+}
+
+class _CustomTextFieldState extends State<DescriptionWidget> {
+  bool showPass = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      spacing: 5,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          widget.title,
+          style: GoogleFonts.poppins(fontWeight: FontWeight.w600, fontSize: 18),
+        ),
+        Container(
+          height: 55,
+          alignment: Alignment.center,
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          decoration: const BoxDecoration(color: Color(0xFFEBEDEF)),
+          child: TextFormField(
+            validator: widget.validator,
+            controller: widget.controller,
+            maxLines: 8,
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              hintText: widget.hint,
+              hintStyle: const TextStyle(fontSize: 13, color: Colors.black45),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
