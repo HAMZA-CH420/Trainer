023cca0dd7c01e31c87bd869ff53a7061c8c7281
Hamza <hakram554r@gmail.com>
2026-04-30 17:11:18 +0500

    changes

--- lib/DataBase/local/db_helper.dart
+++ lib/DataBase/local/db_helper.dart
@@ -66,7 +66,7 @@ class LocalDataBase {
     return rowsAffected > 0 ? userId : null;
   }
 
-  /// this is
+  /// this is a
   ///get current user details
   Future<Map<String, dynamic>?> getUser({required String userId}) async {
     final db = await getDb();
