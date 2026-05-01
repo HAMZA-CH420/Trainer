68c20d032b7ae01f42b57cac00b4a968bd52b7cd
Hamza <hakram554r@gmail.com>
2026-05-01 11:13:05 +0500

    changes

--- lib/DataBase/local/db_helper.dart
+++ lib/DataBase/local/db_helper.dart
@@ -77,7 +77,6 @@ class LocalDataBase {
     return results.isNotEmpty ? results.first : null;
   }
 
-  ///this
   ///add trainers profile in the table
   Future<bool?> addTrainerProfile({
     required String userId,
