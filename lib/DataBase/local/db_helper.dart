a93a285d5fb1e8b9a36e20b99469f818d21f3350
Hamza <hakram554r@gmail.com>
2026-05-03 19:32:52 +0500

    changes

--- lib/DataBase/local/db_helper.dart
+++ lib/DataBase/local/db_helper.dart
@@ -100,7 +100,6 @@ class LocalDataBase {
     return rowsAffected > 0 ? true : false;
   }
 
-  ///this is a comment or
   ///Retrieve trainers profile from the database
   Future<List<Map<String, dynamic>>> getTrainerProfile() async {
     var db = await getDb();
