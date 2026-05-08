4b8df4f05c4fc415924ccb5700a95d5b3e3ee2f8
Hamza <hakram554r@gmail.com>
2026-05-08 12:03:34 +0500

    changes

--- lib/DataBase/local/db_helper.dart
+++ lib/DataBase/local/db_helper.dart
@@ -43,7 +43,7 @@ class LocalDataBase {
     );
   }
 
-  ///this is a comment
+  ///this is a comment for
   ///add a new user in the table
   Future<String?> newUser({
     required String userName,
