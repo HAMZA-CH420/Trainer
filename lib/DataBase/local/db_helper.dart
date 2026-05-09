a8bfe373320f6b88b4a7ee3894767a3c3c349424
Hamza <hakram554r@gmail.com>
2026-05-09 22:53:22 +0500

    changes

--- lib/DataBase/local/db_helper.dart
+++ lib/DataBase/local/db_helper.dart
@@ -17,7 +17,7 @@ class LocalDataBase {
     return db ??= await openDb();
   }
 
-  /// this is
+  /// this is a
   /// open a new database
   Future<Database> openDb() async {
     Directory appDir = await getApplicationDocumentsDirectory();
